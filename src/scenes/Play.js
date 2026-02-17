faa4b89593a7c5261fd48bd599e28530b74e9102
Shiloh Sharmahd <superhumanshiloh@icloud.com>
2026-02-17 00:16:08 -0800

    first commit!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,4 +7,8 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(0, 0, 1080, 768, 'starfield',).setOrigin(0,0)
         this.miner = this.add.tileSprite(leftLaneXPosition, 100, 100, 135, 'miner',).setOrigin(0.5,0.5)
     }
+
+    update() {
+        
+    }
 }
