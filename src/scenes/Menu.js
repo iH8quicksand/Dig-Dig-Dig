bf9bae2fa838cd3ebff4ca0451876a2e6d01f45f
Shiloh Sharmahd <superhumanshiloh@icloud.com>
2026-02-17 17:46:50 -0800

    added L/R logic

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,6 +8,10 @@ class Menu extends Phaser.Scene {
         this.load.image('miner', './assets/miner.png')
     }
 
+    create() {
+        // define keys
+    }
+
     update() {
         this.scene.start('playScene')
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,9 +6,29 @@ class Play extends Phaser.Scene {
     create() {
         this.starfield = this.add.tileSprite(0, 0, 1080, 768, 'starfield',).setOrigin(0,0)
         this.miner = this.add.tileSprite(leftLaneXPosition, 100, 100, 135, 'miner',).setOrigin(0.5,0.5)
+
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
     }
 
     update() {
+        if(Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            if(minerXposition == rightLANE) {
+                minerXposition = middleLANE
+            }
+            if(minerXposition == middleLANE) {
+                minerXposition = leftLANE
+            }
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            if(minerXposition == leftLANE) {
+                minerXposition = middleLANE
+            }
+            if(minerXposition == middleLANE) {
+                minerXposition = rightLANE
+            }
+        }
         
     }
 }
